27243a28f3b59cf6c21ec1ed1409c99b86f20112
Agustín López Zapata <102201642+aguslzz@users.noreply.github.com>
2022-09-03 22:39:34 -0500

    Update main.py
    The main has been updated for testing the integration methods wich are treated equally (as a 1-Dimension array).

--- main.py
+++ main.py
@@ -15,12 +15,22 @@ discret = bar/(bar*10)
 force_mat = process_forces(bar, discret)
 force_with_reactions = calc_reactions_sup(force_mat, si_loc, sf_loc, discret)
 sheer_forces = calc_sheer_forces(force_with_reactions, bar, discret)
+integral_list = trapezoids_sum(discret, sheer_forces[1,:])
+integral_list2 = riemann_sum(discret, sheer_forces[1,:])
 print(sheer_forces)
-x = np.arange(0 ,bar + discret, discret)
 
+
+#Graphing
 plt.plot(sheer_forces[0,:], sheer_forces[1,:])
 plt.xlabel('Variable independiente')
 plt.ylabel('Variable dependiente')
 plt.show()
-
+plt.plot(sheer_forces[0,:], integral_list)
+plt.xlabel('Variable independiente')
+plt.ylabel('Variable dependiente acumulada')
+plt.show()
+plt.plot(sheer_forces[0,:], integral_list2)
+plt.xlabel('Variable independiente')
+plt.ylabel('Variable dependiente acumulada')
+plt.show()
 
